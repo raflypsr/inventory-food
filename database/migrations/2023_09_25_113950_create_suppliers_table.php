2bdb0eea68604622fca463f1bb31971ba935a1d1
raflypermana <124056332+raflypsr@users.noreply.github.com>
2023-12-01 14:30:47 +0700

    Update 2023_09_25_113950_create_suppliers_table.php

--- database/migrations/2023_09_25_113950_create_suppliers_table.php
+++ database/migrations/2023_09_25_113950_create_suppliers_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('suppliers', function (Blueprint $table) {
+        Schema::create('supplier', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('email');
@@ -26,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('suppliers');
+        Schema::dropIfExists('supplier');
     }
 };
